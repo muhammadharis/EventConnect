f9360b5d6ae11f9e51d6b259cfedb47b6120426e
Muhammad Haris <muhammadharis1010@gmail.com>
2018-08-25 19:04:21 -0400

    github database ready

--- src/app/linkedin-page/linkedin-page.component.ts
+++ src/app/linkedin-page/linkedin-page.component.ts
@@ -10,7 +10,7 @@ export class LinkedinPageComponent implements OnInit {
   constructor() { }
 
   linkedin() {
-    window.location.replace("https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=86wrv2s8487eej&redirect_uri=http://localhost:9090/authorizeLinkedin&state=987654321&scope=r_basicprofile");
+    window.location.replace("https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=86wrv2s8487eej&redirect_uri=http://localhost:9090/authorizeLinkedin&state=987654321&scope=r_fullprofile");
     
     //window.open("https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=86wrv2s8487eej&redirect_uri=http://localhost:9090/authorizeLinkedin&state=987654321&scope=r_basicprofile");
   }
