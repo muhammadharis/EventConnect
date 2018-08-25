dbf9303f044871667785c3df36c11f0a06acba87
Muhammad Haris <muhammad.haris@stratus360.com>
2018-08-25 08:45:59 -0400

    Github button

---
+++ server/routes/authorizeLinkedin.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+var request = require('request');
+
+/* GET api listing. */
+router.get('/', (req, res) => {
+    var code = req.query.code;
+    console.log(req.query.code);  
+    request.post({
+    headers: {'content-type' : 'application/x-www-form-urlencoded'},
+    url:     'https://www.linkedin.com/oauth/v2/accessToken?grant_type=authorization_code&code='+code+'&redirect_uri=http://localhost:9090/authorizeLinkedin&client_id=86wrv2s8487eej&client_secret=JR3MWJVoJXT4TWJj',
+    }, function(error, response, body){
+    console.log(JSON.parse(body).access_token);
+    res.end();
+    });
+    
+});
+
+module.exports = router;
