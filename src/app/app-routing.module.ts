e60aafa976c9b2b298bf47e943eef0d67479f3ee
Muhammad Haris <muhammadharis1010@gmail.com>
2018-08-25 13:11:06 -0400

    fixed routing AGAIN for google maps

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LinkedinPageComponent} from './linkedin-page/linkedin-page.component';
 import {UserSelectionComponent} from './user-selection/user-selection.component';
+import {SignUpComponent} from './sign-up/sign-up.component';
 
 const routes:Routes = [
   {path: 'authorizationPage', component: LinkedinPageComponent},
   {path: 'selection', component: UserSelectionComponent},
-  {path: 'messaging', component: UserSelectionComponent}
+  {path: 'messaging', component: UserSelectionComponent},
+  {path: 'signup', component: SignUpComponent}
 ]
 
 @NgModule({
